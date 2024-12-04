84fcb4868092a391b2dc25a1fa0fc47a1f581be2
srinivasmeda-brs <svas77310@gmail.com>
2024-12-04 11:23:23 +0530

    third

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
-    host: '0.0.0.0', // Make the server accessible externally
-    port: 4173,  // Ensure this port is used for your production build
+    host: '0.0.0.0',
+    port: process.env.PORT || 80,  // Ensure the app binds to the correct port
   },
   build: {
-    outDir: 'dist', // Make sure the build output goes into the 'dist' folder
+    outDir: 'dist',
   },
 });
